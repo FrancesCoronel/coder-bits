b64dd264a8ec1f3c59228f9200e4a3c8e5908195
FVCproductions <fvcproductions@users.noreply.github.com>
2014-06-13 17:46:41 -0400

    Create 2.3.py

---
+++ Python-Bits/More-with-Input-Output-and-Data-Operations/2.3.py
@@ -0,0 +1,12 @@
+"""
+
+Problem 2.3 - Time calculation I
+
+Write a program where you can enter from keyboard values for hours and minutes. Your program should compute and print on screen resulting total number of minutes.
+
+"""
+
+hour = int(input("Enter hour: "))
+minute = int(input("Enter minute: "))
+
+print("total minutes: ", hour*60+minute)

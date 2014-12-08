6834a01d4fbcd1359c6c85c5bef123626ffe36ed
FVCproductions <fvcproductions@icloud.com>
2014-12-08 10:12:30 -0500

    double linked list addition

--- Java-Bits/Part-III/LinkedLists/DoublyLinkedList/DLinkListFC.java
+++ Java-Bits/Part-III/LinkedLists/DoublyLinkedList/DLinkListFC.java
@@ -1,39 +1,66 @@
 /*
 
-Frances
+FRANCES CORONEL
 CSC 251
+Data Structures
 November 20, 2014
-Detailed Description of each file and what it is supposed to do
 
-"I Frances have completed this program individually without any student or faculty support. The penalty for not completing this program individually is a zero for the program and admission of CHEATING in my student file."
+Detailed Description:
+
+	- DLinkListFC is the driver of this program and has the main method which reads from a text file called list.txt and executes each text commmand. 
+ 	- Comments are strewn throughout.
+	- This particular driver utilizes the following possible commands for the double linked list:
+		- clear()
+		- isEmpty()
+		- size()
+		- add(String aString)
+		- get(String aString)
+		- remove(String aString)
+		- printForward()
+		- printReverse()
+	- Has 2 Exceptions within the try catch clauses that are called for the remove() and get() methods
+		- ItemNotPresentException - throws error if item is not in list
+		- EmptyListException - throws error if list is empty
+
+"I, FRANCES CORONEL, have completed this program individually without any student or faculty support. 
+The penalty for not completing this program individually is a zero for the program and admission of CHEATING in my student file."
 
 */
 
-import java.util.Scanner;
-import java.io.*;
-import java.util.StringTokenizer;
+// importing necessary classes
+
+import java.util.Scanner; //Scanner class
+import java.io.*; //input output
+import java.util.StringTokenizer; //String Tokenizer class
 
 public class DLinkListFC {
 	
 	public static void main(String[] args) throws IOException {
 		
-		MyLinkedListFC myList = new MyLinkedListFC();
+		MyLinkedListFC myList = new MyLinkedListFC(); //creating new object using class file
 		
-		String command;
+		String command; //commands are read from each line from the text file
 		String stringWord = "";					
 		
-		File inputFile = new File("list.txt");
-		Scanner readFile = new Scanner(inputFile);
+		//reading file with scanner called 'list.txt'
+		
+		File inputFile = new File("list.txt"); //file being defined as input file
+		Scanner readFile = new Scanner(inputFile); //scanner to read file
 	
+	
+		//reading line by line in file with while loop
+		
 		while(readFile.hasNext()) {
 		
 			String newLine = readFile.nextLine();
+			
+			//using string tokenizer to get text command from each line
 			
 			StringTokenizer st = new StringTokenizer(newLine);
 			command = st.nextToken();
 			System.out.print("Command: " + command);
 			
-			if (st.hasMoreTokens()) {
+			if (st.hasMoreTokens()) { //if there are more words after command
 				
 				stringWord = st.nextToken();
 				System.out.print(" " + stringWord);
@@ -42,110 +69,136 @@ public class DLinkListFC {
 			
 			System.out.println();
 			
+			// command = add
+			
 			if(command.equals("add")) { //had to complete
 			
 				myList.add(stringWord);
+				System.out.println();
 			
 			}
+			
+			// command = size
 			
 			else if(command.equals("size")) { //had to complete
 				
-				myList.size();
+				System.out.println(myList.size());
+				System.out.println();
 				
 			}
+			
+			// command = printForward
 			
 			else if(command.equals("printForward")) { //had to complete
 				
 				myList.printForward();
+			
 			}
 
+			// command = printReverse
+			
 			else if(command.equals("printReverse")) { //had to complete
 			
 				myList.printReverse();
 				
 			}
 
+			// command = isEmpty
+			
 			else if(command.equals("isEmpty")) { //had to complete
 				
 				if(myList.isEmpty()) {
 					
-					System.out.println("List is empty.");
+					System.out.println("List is empty."); //prints out statement that list is empty
+					System.out.println();
 				
 				}
 				
 				else {
 				
-					System.out.println("List is not empty.");
-				
-				}
-			}
-			
-			else if(command.equals("get")) {
-				
-				try {
-					
-					myList.EmptyListException();
-					
-					try {
-						
-						myList.ItemNotPresentException(stringWord);
-						System.out.print("\t" + myList.get(stringWord));
-						System.out.println("  ***Successful!***");
-						System.out.println();
-					}
-					
-					catch(MyLinkedListExceptionFC notPresent) {
-						
-						System.out.println("\t***Unsuccessful*** \n" + notPresent.getMessage());
-						System.out.print("List: ");
-						myList.printForward();
-						System.out.println();
-					
-					}	
-				}
-				
-				catch(MyLinkedListExceptionFC underflow) {
-					
-					System.out.println("\t***Unsuccessful*** \n" + underflow.getMessage());
+					System.out.println("List is not empty."); //prints out statement that list is not empty
 					System.out.println();
 				
 				}
+			}
+			
+			//if command is get, checking for certain things
+			
+			else if(command.equals("get")) { //if the text command in list is 'get', then...
+				
+				try
+				{
+					myList.EmptyListException(); //checking if list is empty									
+               try
+					{
+						myList.ItemNotPresentException(stringWord); //checking if item is not present in list
+						System.out.print("\t" + myList.get(stringWord)); // if not empty or item is present, then successful message appears
+						System.out.println("  ***Successful!***");
+						System.out.println();
+					}
+					catch(MyLinkedListExceptionFC notPresent) //if item not present, prints unsuccessful message
+					{
+						System.out.println("\t***Unsuccessful*** \n" + notPresent.getMessage());		
+						System.out.print("List: ");
+						myList.printForward();
+						
+					}
+				}
+				catch(MyLinkedListExceptionFC underflow) //if list is empty, prints unsuccessful message
+				{
+					System.out.println("\t***Unsuccessful*** \n" + underflow.getMessage());
+					System.out.println();
+				}
 			
 			}
+			
+			// if command is remove, check for certain things
 
 			else if(command.equals("remove")) { //had to complete
 				
 				try {
-					
-					myList.EmptyListException();
+								
+					myList.EmptyListException(); //checking if list is empty
 					
 					try {
-
+						
+						myList.ItemNotPresentException(stringWord); //checking if item is not present in list
+						System.out.print("\t" + myList.remove(stringWord)); // if not empty or item is present, then successful message appears
+						System.out.println("  ***Successful!***");
+						System.out.println();
 					}
 					
-					catch(MyLinkedListExceptionFC notPresent) {
-               
+					catch(MyLinkedListExceptionFC notPresent) { //if item not present, prints unsuccessful message
+						
 						System.out.println("\t***Unsuccessful*** \n" + notPresent.getMessage());
 						System.out.print("List: ");
 						myList.printForward();
 						System.out.println();
 					
 					}
+
 				}
 				
-				catch(MyLinkedListExceptionFC underflow) {
-               
+				catch(MyLinkedListExceptionFC underflow) { //if list is empty, prints unsuccessful message
+					
 					System.out.println("\t***Unsuccessful*** \n" + underflow.getMessage());
 					System.out.println();
 				
 				}
+	
 			}
+			
+			// command is clear
 			
 			else if(command.equals("clear")) { //had to complete
       			
-				myList.clear();
-			
+				myList.clear(); //calling myList clear method
+				System.out.println("List has been cleared.");
+				System.out.println();
+				
 			}
+			
+			// if command is none of the above, then error message!
 
 			else {
 				
@@ -156,7 +209,14 @@ public class DLinkListFC {
 	
 		}
 		
-		readFile.close();				
+		//closing file
+		
+		readFile.close();
+		
+		//necessary statement
+		
+		System.out.println("I, FRANCES CORONEL, have completed this program individually without any student or faculty support.");
+		System.out.println("The penalty for not completing this program individually is a zero for the program and admission of CHEATING in my student file.");				
 	
 	}
 }

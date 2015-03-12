21cc83d0929e20ea4029ed8cba496075cd91b36d
FVCproductions <fvcproductions@icloud.com>
2015-03-11 22:28:00 -0400

    HW4 Assignment Updated
    Signed-off-by: FVCproductions <fvcproductions@icloud.com>

--- Java-Bits/Part-IV/HW4UnionFind/DisjointSets.java
+++ Java-Bits/Part-IV/HW4UnionFind/DisjointSets.java
@@ -1,4 +1,4 @@
-/*
+/**
  * Interface for disjoint sets implementations for ints. 
  * Assumes elements in the set are unique and numbered starting at 0, 
  * ending at numElements-1.  Set names are also in this same range.
@@ -7,7 +7,7 @@
 public interface DisjointSets 
 {
 
-    /*
+    /**
      * Union (combine) two disjoint sets into one set. 
      * No restriction on which set should be added to the other set.
      * @param set1 the name of set 1.
@@ -17,7 +17,7 @@ public interface DisjointSets
      */
     public void union(int set1, int set2);
 
-    /*
+    /**
      * Find which set element x belongs to.
      * @param x the element being searched for.
      * @return the name of the set containing x.
@@ -25,13 +25,13 @@ public interface DisjointSets
      */
     public int find(int x);
 
-    /*
+    /**
      * Returns the current total number of sets.
      * @return the current number of sets.
      */
     public int numSets();
 
-    /*
+    /**
      * Determine if an element is the name of a set.
      * @param x an element
      * @return true if x is the name of a set
@@ -39,14 +39,14 @@ public interface DisjointSets
      */
     public boolean isSetName(int x);
 
-    /*
+    /**
      * Returns the total number of elements in the given set.
      * @param setNum the name of a set
      * @throws InvalidSetNameException if setNum is not the name of a set.
      */
     public int numElements(int setNum);
 
-    /*
+    /**
      * Prints out the elements in the given set.
      * setNum is assumed to be a root and represents the name of a set.
      * @param setNum the name of a set
@@ -54,7 +54,7 @@ public interface DisjointSets
      */
     public void printElements(int setNum);
 
-    /*
+    /**
      * Returns an array containing the elements in the given set.
      * @param setNum the name of a set
      * @returns an array containing the elements in the given set.
